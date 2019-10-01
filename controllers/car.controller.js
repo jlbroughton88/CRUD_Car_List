510cd0f27742c250a41e2cc3bdb52625647a0008
jlb1999 <jlbroughton88@gmail.com>
2019-10-01 17:26:18 -0400

    fixed heroku deploy issue, now trying to fix routing issue

--- controllers/car.controller.js
+++ controllers/car.controller.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const db = mongoose.connection;
 
 exports.car_get_all = (req, res) => {
+    res.send("Hello")
     db.model("Car").find({}, (error, result) => {
         if(error) { console.log(error) }
         else { res.send(result) }
