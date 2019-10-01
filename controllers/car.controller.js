2ec30c7905497fc3d9d46a0a321cf1506b1b7549
jlb1999 <jlbroughton88@gmail.com>
2019-10-01 17:59:56 -0400

    test

--- controllers/car.controller.js
+++ controllers/car.controller.js
@@ -3,11 +3,7 @@ const mongoose = require("mongoose");
 const db = mongoose.connection;
 
 exports.car_get_all = (req, res) => {
-    res.send("Hello")
-    console.log("1 >>>>>>>>>>" + res)
-    console.log("--------------------------------------")
-    console.log("2 >>>>>>>>>>" + db)
-    console.log("--------------------------------------")
+    res.send(db)
     console.log("3 >>>>>>>>>>" + db.model("Car"))
     db.model("Car").find({}, (error, result) => {
         if(error) { console.log(error) }
